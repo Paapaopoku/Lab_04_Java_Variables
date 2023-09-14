e2ed61d0ff3759173aecd620b9d5819f5991f2c2
Hello world <Opokukka@mail.uc.edu>
2023-09-14 13:16:16 -0400

    Num

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
 public class Main
 {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        // intOperandA, intOperandB, intSum, intProduct, intDifference, intQuotient, intModulo
+        int  intOperandA = 1;
+        int intOperandB = 6;
+        int intSum = 0;
+        int intProduct = 8;
+
+
+
+
+
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
+
+        // rest of the int statement go here
+
+        double doubleOperandA = 20;
+        double doubleOperandB = 7;
+        double doubleSum = doubleOperandA + doubleOperandB;
+
+        System.out.println("The sum of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum );
     }
 }
